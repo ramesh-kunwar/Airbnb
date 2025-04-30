1d2bc8c626ffe0671fb21dede469e56cf1608b7a
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2025-04-30 17:06:09 +0545

    Added migrations for creating table

---
+++ HotelService/src/config/config.js
@@ -0,0 +1,19 @@
+// import { dbConfig } from "./index";
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const config = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  },
+};
+
+// module.exports = config;
+
+// export config
+module.exports = config;
--- HotelService/src/config/index.ts
+++ HotelService/src/config/index.ts
@@ -5,6 +5,13 @@ type ServerConfig = {
   PORT: number;
 };
 
+type DBConfig = {
+  DB_HOST: string;
+  DB_USER: string;
+  DB_PASSWORD: string;
+  DB_NAME: string;
+};
+
 export function loadEnv() {
   dotenv.config();
   console.log(`Environment variables loaded`);
@@ -12,4 +19,11 @@ export function loadEnv() {
 loadEnv();
 export const serverConfig: ServerConfig = {
   PORT: Number(process.env.PORT) || 4000,
+};
+
+export const dbConfig: DBConfig = {
+  DB_HOST: process.env.DB_HOST || "localhost",
+  DB_USER: process.env.DB_USER || "root",
+  DB_PASSWORD: process.env.DB_PASSWORD || "rootUser",
+  DB_NAME: process.env.DB_NAME || "test_db",
 };
